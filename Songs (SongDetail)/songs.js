7b0b0b8da335b158fb4ae3e6f9129f19aec4d5cb
melio <dhitanimam05@gmail.com>
2024-06-01 15:57:22 +0700

    malamMinggu

---
+++ Songs (SongDetail)/songs.js
@@ -0,0 +1,51 @@
+function redirectToHEYA(){
+    window.location.href = '../../Songs (SongDetail)/Heya/heya.html';
+}
+function redirectToAbyss(){
+    window.location.href = '../../Songs (SongDetail)/Abyss/abyss.html';
+}
+function redirectToDramaturgy(){
+    window.location.href = '../../Songs (SongDetail)/Dramaturgy/dramaturgy.html';
+}
+function redirectToFortnight(){
+    window.location.href = '../../Songs (SongDetail)/Fortnight/fortnight.html';
+}
+function redirectToFriends(){
+    window.location.href = '../../Songs (SongDetail)/Fri(end)s/friends.html';
+}
+function redirectToGhost(){
+    window.location.href = '../../Songs (SongDetail)/Ghost/ghost.html';
+}
+function redirectToHelena(){
+    window.location.href = '../../Songs (SongDetail)/Helena/helena.html';
+}
+function redirectToLoveWinsAll(){
+    window.location.href = '../../Songs (SongDetail)/LoveWinsAll/love.html';
+}
+function redirectToMaestro(){
+    window.location.href = '../../Songs (SongDetail)/Maestro/maestro.html';
+}
+function redirectToNotLikeUs(){
+    window.location.href = '../../Songs (SongDetail)/NotLikeUs/NotLikeUs.html';
+}
+function redirectToOO(){
+    window.location.href = '../../Songs (SongDetail)/OOEnmik/oo.html';
+}
+function redirectToHowSweet(){
+    window.location.href = '../../Songs (SongDetail)/HowSweet/howsweet.html';
+}
+function redirectToStereotype(){
+    window.location.href = '../../Songs (SongDetail)/Stereotype/stereotype.html';
+}
+function redirectToSupernova(){
+    window.location.href = '../../Songs (SongDetail)/Supernova/supernova.html';
+}
+function redirectToYOLO(){
+    window.location.href = '../../Songs (SongDetail)/YOLO/yolo.html';
+}
+function redirectToLoveDive(){
+    window.location.href = '../../Songs (SongDetail)/LoveDive/lovedive.html';
+}
+function redirectToEndroll(){
+    window.location.href = '../../Songs (SongDetail)/Endroll/endroll.html';
+}
